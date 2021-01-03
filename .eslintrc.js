ccb61250ea1fc17963fd513938760ca3d9add1e1
Shaun Starsprung <s@starsprung.com>
2021-01-03 12:55:56 -0800

    ci: update test platforms in GHA

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
   env: {
-    es2020: true
+    es2020: true,
   },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
-    'no-empty': 'off'
-  }
+    'no-empty': 'off',
+  },
 };
