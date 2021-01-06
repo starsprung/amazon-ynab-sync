f979f1d8382117c134294a4885988d2051dc0246
Shaun Starsprung <starsprung@users.noreply.github.com>
2021-01-05 22:10:38 -0800

    fix: add email address to support notice

--- src/support-notice.ts
+++ src/support-notice.ts
@@ -4,7 +4,7 @@ const SUPPORT_NOTICE = `
 If you'd like to support this project, please consider donating to Oregon Food Bank: ${chalk.bold(
   'https://oregonfoodbank.org',
 )}
-Donate more than $50 and send me a screenshot of your donation and I'll add you to a list of supporters!
+Donate more than $50 and send me (s@starsprung.com) a screenshot of your donation and I'll add you to a list of supporters!
 `;
 
 const supportNotice = (): void => {
